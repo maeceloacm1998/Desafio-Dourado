32fb57e278359a01e69f7b25f7486dd95c0913f7
marcelochaves <mcmendes@uolinc.com>
2023-11-25 03:00:54 -0300

    feat: add TopTabRow

--- app/src/main/java/com/app/desafiodourado/feature/home/ui/HomeScreen.kt
+++ app/src/main/java/com/app/desafiodourado/feature/home/ui/HomeScreen.kt
@@ -1,10 +1,20 @@
 package com.app.desafiodourado.feature.home.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,56 +23,127 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import com.app.desafiodourado.components.background.Background
 import com.app.desafiodourado.components.coinview.CoinView
+import com.app.desafiodourado.components.toolbar.ToolbarCustom
+import com.app.desafiodourado.ui.theme.Background
+import com.app.desafiodourado.ui.theme.BrowLight
 import com.app.desafiodourado.ui.theme.CustomDimensions
+import com.app.desafiodourado.ui.theme.PurpleLight
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    HomeComponents()
+    val tabs = listOf("Desafios Pendentes", "Desafios Concluidos")
+    HomeComponents(tabs = tabs)
 }
 
 @Composable
-fun HomeComponents() {
+fun HomeComponents(tabs: List<String>) {
     Background {
-        ConstraintLayout(
+        Column(
             modifier = Modifier.fillMaxSize()
         ) {
-            val (txtVersion, coinView) = createRefs()
-            createHorizontalChain(txtVersion, coinView, chainStyle = ChainStyle.SpreadInside)
-            Text(
-                modifier = Modifier
-                    .padding(
-                        horizontal = CustomDimensions.padding20,
-                        vertical = CustomDimensions.padding14
-                    )
-                    .constrainAs(txtVersion) {
-                        end.linkTo(coinView.start)
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                    },
-                text = "v1.0.0",
-                style = MaterialTheme.typography.titleMedium,
-                color = Color.White
-            )
+            TopBar(coin = 1245)
+            ToolbarCustom(title = "Premios Misteriosos", badgeCount = 2) {
+                // TODO abrir modal com os desagios
+            }
+            TopTabRow(tabs = tabs) {
 
-            CoinView(
-                modifier = Modifier
-                    .padding(
-                        horizontal = CustomDimensions.padding20,
-                        vertical = CustomDimensions.padding14
-                    )
-                    .constrainAs(coinView) {
-                        end.linkTo(parent.end)
-                        top.linkTo(parent.top)
-                        start.linkTo(txtVersion.end)
+            }
+        }
+    }
+}
+
+@Composable
+fun TopBar(coin: Int) {
+    ConstraintLayout(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        val (txtVersion, coinView) = createRefs()
+        createHorizontalChain(txtVersion, coinView, chainStyle = ChainStyle.SpreadInside)
+        Text(
+            modifier = Modifier
+                .padding(
+                    horizontal = CustomDimensions.padding20,
+                    vertical = CustomDimensions.padding14
+                )
+                .constrainAs(txtVersion) {
+                    end.linkTo(coinView.start)
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                },
+            text = "v1.0.0",
+            style = MaterialTheme.typography.titleMedium,
+            color = Color.White
+        )
+
+        CoinView(
+            modifier = Modifier
+                .padding(
+                    horizontal = CustomDimensions.padding20,
+                    vertical = CustomDimensions.padding14
+                )
+                .constrainAs(coinView) {
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top)
+                    start.linkTo(txtVersion.end)
+                },
+            coin = coin
+        )
+    }
+}
+
+@Composable
+fun TopTabRow(tabs: List<String>, onClickTabOptionListener: (position: Int) -> Unit) {
+    var state by remember { mutableStateOf(0) }
+
+    ConstraintLayout(modifier = Modifier.padding(top = CustomDimensions.padding20)) {
+        TabRow(
+            selectedTabIndex = state,
+            containerColor = Background,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier
+                        .tabIndicatorOffset(tabPositions[state]),
+                    height = CustomDimensions.padding2,
+                    color = BrowLight
+                )
+            }
+        ) {
+            tabs.forEachIndexed { position, title ->
+                Tab(
+                    selected = state == position,
+                    onClick = {
+                        state = position
+                        onClickTabOptionListener(position)
                     },
-                coin = 1205
-            )
+                    text = {
+                        Text(
+                            text = title,
+                            color = if (position != state) PurpleLight else BrowLight,
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                    }
+                )
+            }
         }
     }
 }
 
 @Preview
 @Composable
+fun TopTabRowPreview() {
+    val titles = listOf("Desafios Pendentes", "Desafios Concluidos")
+    TopTabRow(tabs = titles) {}
+}
+
+@Preview
+@Composable
 fun HomePreview() {
-    HomeComponents()
+    val titles = listOf("Desafios Pendentes", "Desafios Concluidos")
+    HomeComponents(tabs = titles)
+}
+
+@Preview
+@Composable
+fun TopBarPreview() {
+    TopBar(coin = 4000)
 }
