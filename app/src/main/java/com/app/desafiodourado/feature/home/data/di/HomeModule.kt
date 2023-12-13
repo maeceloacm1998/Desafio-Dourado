0a4f7fc6ec42a04a1172d7a8a841d4f98b86b7d0
marcelochaves <mcmendes@uolinc.com>
2023-12-13 17:29:37 -0300

    fix: injection update quantity coins use case

--- app/src/main/java/com/app/desafiodourado/feature/home/data/di/HomeModule.kt
+++ app/src/main/java/com/app/desafiodourado/feature/home/data/di/HomeModule.kt
@@ -4,6 +4,7 @@ import com.app.desafiodourado.feature.home.data.HomeRepository
 import com.app.desafiodourado.feature.home.data.HomeRepositoryImpl
 import com.app.desafiodourado.feature.home.domain.StartCountDownUseCase
 import com.app.desafiodourado.feature.home.domain.UpdateMissionsUseCase
+import com.app.desafiodourado.feature.home.domain.UpdateQuantityCoinsUseCase
 import com.app.desafiodourado.feature.home.ui.HomeViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -12,6 +13,7 @@ object HomeModule {
     val modules = module {
         factory<HomeRepository> { HomeRepositoryImpl(client = get(), accountManager = get()) }
         factory { UpdateMissionsUseCase(homeRepository = get()) }
+        factory { UpdateQuantityCoinsUseCase(accountManager = get()) }
         factory { StartCountDownUseCase(timerManager = get()) }
         viewModel {
             HomeViewModel(
